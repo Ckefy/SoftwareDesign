d538aae7e7e84c6f74aed436cf2aec0ed152a3d6
Arseny Lukonin <lukonin004@gmail.com>
2021-10-10 21:20:00 +0300

    chore: delete unnecessary imports

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -2,16 +2,10 @@ package ru.akirakozov.sd.refactoring.servlet;
 
 import ru.akirakozov.sd.refactoring.entities.product.dao.ProductDAO;
 import ru.akirakozov.sd.refactoring.entities.product.dto.ProductDTO;
-import ru.akirakozov.sd.refactoring.entities.product.printer.HTMLPrinter;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServlet.java
@@ -3,15 +3,9 @@ package ru.akirakozov.sd.refactoring.servlet;
 import ru.akirakozov.sd.refactoring.entities.product.dao.ProductDAO;
 import ru.akirakozov.sd.refactoring.entities.product.printer.HTMLPrinter;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
